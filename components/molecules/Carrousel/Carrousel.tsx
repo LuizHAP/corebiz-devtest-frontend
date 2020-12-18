999ea3fd314b208c0843c8529ee99f5b874bae1c
Luiz Henrique <lpansarini@gmail.com>
2020-12-18 20:02:59 -0300

    feat(carrousel): create carrousel component using images and svg

---
+++ components/molecules/Carrousel/Carrousel.tsx
@@ -0,0 +1,21 @@
+import styles from "./Carrousel.module.css";
+
+const Carrousel = () => {
+  return (
+    <div className={styles.root}>
+      <img src="imageBanner.png" />
+      <div className={styles.carrouselCaption}>
+        <h2>Olá, o que você está buscando?</h2>
+        <h1>Criar ou migrar seu e-commerce?</h1>
+      </div>
+      <div className={styles.dots}>
+        <span className={styles.dotActive} />
+        <span />
+        <span />
+        <span />
+      </div>
+    </div>
+  );
+};
+
+export default Carrousel;
--- components/organisms/MainSection/MainSection.tsx
+++ components/organisms/MainSection/MainSection.tsx
@@ -1,9 +1,11 @@
+import Carrousel from "@components/molecules/Carrousel";
+
 import Link from "next/link";
 
 const MainSection = () => {
   return (
     <main>
-      <h2>Main Section</h2>
+      <Carrousel />
     </main>
   );
 };
