d042f65b2ec9c1cff8466a4d575b48df34cbd79f
Luiz Henrique <lpansarini@gmail.com>
2020-12-18 20:13:06 -0300

    fix(structure): fix main tag and hero section with carousel

---
+++ components/organisms/HeroSection/HeroSection.tsx
@@ -0,0 +1,7 @@
+import Carrousel from "@components/molecules/Carrousel";
+
+const HeroSection = () => {
+  return <Carrousel />;
+};
+
+export default HeroSection;
--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -1,1 +1,1 @@
-export { default as MainSection } from './MainSection'
+export { default as HeroSection } from './HeroSection'
